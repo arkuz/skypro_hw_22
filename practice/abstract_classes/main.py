0dc999942c2ba8ce190e07c8ff779db16a0ac147
Artem Kuzmenko <arkuzka@mail.ru>
2022-09-27 13:43:37 +0300

    init project

---
+++ practice/abstract_classes/main.py
@@ -0,0 +1,84 @@
+from abc import ABC, abstractmethod
+
+
+class Transport(ABC):
+    @abstractmethod
+    def start_engine(self):
+        pass
+
+    @abstractmethod
+    def stop_engine(self):
+        pass
+
+    @abstractmethod
+    def move(self):
+        pass
+
+    @abstractmethod
+    def stop(self):
+        pass
+
+
+class Boat(Transport):
+    def start_engine(self):
+        print('Катер громко забурчал')
+
+    def stop_engine(self):
+        print('Двигатель катера чихнул напоследок и заглох')
+
+    def move(self):
+        print('Катер быстро набирает скорость')
+
+    def stop(self):
+        print('Катер остановился')
+
+
+class Car(Transport):
+    def start_engine(self):
+        print('Машина заурчала двигателем')
+
+    def stop_engine(self):
+        print('Машина стоит с заглушенным двигателем')
+
+    def move(self):
+        print('Машина едет к цели назначения')
+
+    def stop(self):
+        print('Машина остановилась')
+
+
+class Electroscooter(Transport):
+    def start_engine(self):
+        print('Мигнул светодиодом')
+
+    def stop_engine(self):
+        print('Мигнул светодиодом дважды')
+
+    def move(self):
+        print('Прохожие в ужасе разбегаются от очередного камикадзе')
+
+    def stop(self):
+        print('Торможение об стену прошло успешно')
+
+
+class Person:
+    def use_transport(self, transport):
+        transport.start_engine()
+        transport.move()
+        transport.stop()
+        transport.stop_engine()
+
+
+# Отрезок кода для самопроверки.
+# Запустите его, после того как выполните задание
+if __name__ == '__main__':
+    boat = Boat()
+    car = Car()
+    kamikadze = Electroscooter()
+
+    person = Person()
+    person.use_transport(boat)
+    print('=' * 20)
+    person.use_transport(car)
+    print('=' * 20)
+    person.use_transport(kamikadze)
